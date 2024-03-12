8d41518a8c06e6a0f046d5058645f68affebbb23
Alex <alexmatvichuk@gmail.com>
2024-03-12 09:52:47 +0100

    Add Style for Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@ import AddCompanyButton from '@components/Company/AddCompanyButton';
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="m-0 text-xl">HOME PAGE</h1>
-
+    <main className="flex justify-center items-center h-lvh flex-col">
+      <h1 className="mb-8 text-xl">HOME PAGE</h1>
       <AddCompanyButton></AddCompanyButton>
     </main>
   );
